8469c96644c70427c344d2bc394a5646c67209b6
youdavo <youdavo@gmail.com>
2015-01-07 19:33:14 +0900

    Finish user edit, update, index, add destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+before_action :signed_in_user, 	only: [:index, :edit, :update, :destroy]
+before_action :correct_user,	only: [:edit, :update]
+before_action :admin_user,		only: :destroy
+
+	def index
+		@users = User.paginate(page: params[:page])
+	end
 
 	def show
 		@user = User.find(params[:id])
@@ -12,11 +19,29 @@ class UsersController < ApplicationController
 		@user = User.new(user_params)
 		if @user.save
 			sign_in @user
-			flash[:success] = "Welcome to the Sample App!"
+			flash[:success] = "Welcome to Sample App! ご登録いただき有り難うございます。"
 			redirect_to @user
 		else
 			render 'new'
 		end
+	end
+
+	def edit
+	end
+
+	def update
+		if @user.update_attributes(user_params)
+			flash[:success] = "プロファイルが更新されました。"
+			redirect_to @user
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		User.find(params[:id]).destroy
+		flash[:success] = "User destroyed."
+		redirect_to users_url
 	end
 
 	private
@@ -25,4 +50,21 @@ class UsersController < ApplicationController
 			params.require(:user).permit(:name, :email, :password, :password_confirmation)
 		end
 
+		# Before action
+
+		def signed_in_user
+			unless signed_in?
+				store_location
+				redirect_to signin_url, notice: "サインインしてください。" 
+			end
+		end
+
+		def correct_user
+			@user = User.find(params[:id])
+			redirect_to(root_path) unless current_user?(@user)
+		end
+
+		def admin_user
+			redirect_to(root_path) unless current_user.admin?
+		end
 end
